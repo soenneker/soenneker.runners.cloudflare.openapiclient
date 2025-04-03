af6c03abe614a127a03501c0accd02919d204a30
Jake Soenneker <jake@soenneker.com>
2025-04-02 19:21:43 -0500

    Fixes test

--- test/Soenneker.Runners.Cloudflare.OpenApiClient.Tests/CloudflareOpenApiClientTests.cs
+++ test/Soenneker.Runners.Cloudflare.OpenApiClient.Tests/CloudflareOpenApiClientTests.cs
@@ -1,5 +1,4 @@
-﻿using Soenneker.Runners.Cloudflare.OpenApiClient.Abstract;
-using Soenneker.Tests.FixturedUnit;
+﻿using Soenneker.Tests.FixturedUnit;
 using Xunit;
 
 namespace Soenneker.Runners.Cloudflare.OpenApiClient.Tests;
@@ -7,11 +6,8 @@ namespace Soenneker.Runners.Cloudflare.OpenApiClient.Tests;
 [Collection("Collection")]
 public class CloudflareOpenApiClientTests : FixturedUnitTest
 {
-    private readonly ICloudflareOpenApiClient _runner;
-
     public CloudflareOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
-        _runner = Resolve<ICloudflareOpenApiClient>(true);
     }
 
     [Fact]
