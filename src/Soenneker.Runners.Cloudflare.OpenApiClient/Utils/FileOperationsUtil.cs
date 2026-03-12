22256a29b5b0a9b3f647dd973d306406ca32aa03
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-03-11 20:14:03 -0500

    organization, improves CI

--- src/Soenneker.Runners.Cloudflare.OpenApiClient/Utils/FileOperationsUtil.cs
+++ src/Soenneker.Runners.Cloudflare.OpenApiClient/Utils/FileOperationsUtil.cs
@@ -67,7 +67,7 @@ public sealed class FileOperationsUtil : IFileOperationsUtil
         await _openApiFixer.Fix(filePath, fixedFilePath, cancellationToken)
                            .NoSync();
 
-        string srcDirectory = Path.Combine(gitDirectory, "src");
+        string srcDirectory = Path.Combine(gitDirectory, "src", Constants.Library);
 
         await DeleteAllExceptCsproj(srcDirectory, cancellationToken);
 
@@ -82,7 +82,7 @@ public sealed class FileOperationsUtil : IFileOperationsUtil
         await FixExpiryDefault(srcDirectory, cancellationToken);
         await FixStringListDefaults(srcDirectory, cancellationToken);
 
-        string projFilePath = Path.Combine(gitDirectory, "src", $"{Constants.Library}.csproj");
+        string projFilePath = Path.Combine(gitDirectory, "src", Constants.Library, $"{Constants.Library}.csproj");
 
         await _dotnetUtil.Restore(projFilePath, cancellationToken: cancellationToken);
 
@@ -250,7 +250,7 @@ public sealed class FileOperationsUtil : IFileOperationsUtil
 
     private async ValueTask BuildAndPush(string gitDirectory, CancellationToken cancellationToken)
     {
-        string projFilePath = Path.Combine(gitDirectory, "src", $"{Constants.Library}.csproj");
+        string projFilePath = Path.Combine(gitDirectory, "src", Constants.Library, $"{Constants.Library}.csproj");
 
         await _dotnetUtil.Restore(projFilePath, cancellationToken: cancellationToken);
 
